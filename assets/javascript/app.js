c24d95e25e19b403c360cd97817cc89e3b8b33e9
emilyfjonaitis <emilyfjonaitis@gmail.com>
2019-03-11 22:30:24 -0600

    another try

---
+++ assets/javascript/app.js
@@ -0,0 +1,196 @@
+$(document).ready(function () {
+    var options = [{
+            question: "How many houses are there at Hogwarts?",
+            choice: ["1", "2", "3", "4"],
+            answer: 3,
+            photo: "assets/images/1question.jpg"
+        },
+        {
+            question: "Who was Harry Potters best friend?",
+            choice: ["Ron Weasley", "Hermione Granger", "Draco Malfoy", "Dobby"],
+            answer: 0,
+            photo: "assets/images/2question.jpg"
+        },
+        {
+            question: "Who was Harry Potter's God Father?",
+            choice: ["Serious Black", "Rubeus Hagrid", "Snape", "DoumbleDore"],
+            answer: 0,
+            photo: "assets/images/3question.jpg"
+        },
+        {
+            question: "What is NOT the name of a house at Hogwarts?",
+            choice: ["RavenClaw", "Ravenbeak", "Griffindore", "Hufflepuff"],
+            answer: 1,
+            photo: "assets/images/4question.jpg"
+        },
+        {
+            question: "What the Sorting Hats job?",
+            choice: ["Answers the questions that are in you heart", "tells the future", "Sorts the animals into the house they belong to", "Sorts students into specific houses, based off whats in the concious"],
+            answer: 3,
+            photo: "assets/images/5question.jpg"
+        },
+        {
+            question: "What is a Boggart?",
+            choice: ["Tells your future", "Makes your worst nightmare come to life", "Show you your most wanted thing in life", "Shows you the people who you love the most"],
+            answer: 1,
+            photo: "assets/images/6question.jpg"
+        },
+        {
+            question: "What does the Patronus Spell do?",
+            choice: ["shows you thing you fear the most", "Cast animal gaurdian that is sent to help you", "lift stuff up and make it weightless", "makes you invisable"],
+            answer: 1,
+            photo: "assets/images/7question.jpg"
+        },
+        {
+            question: "What was Lord Voldemort's evil pet snakes name?",
+            choice: ["Nagini", "Trixxel", "Dolores", "Albus"],
+            answer: 0,
+            photo: "assets/images/8question.jpg"
+        }
+    ];
+
+    var correctCount = 0;
+    var wrongCount = 0;
+    var unanswerCount = 0;
+    var timer = 20;
+    var intervalId;
+    var userGuess = "";
+    var running = false;
+    var qCount = options.length;
+    var pick;
+    var index;
+    var newArray = [];
+    var holder = [];
+
+
+
+    $("#reset").hide();
+    //click start button to start game
+    $("#start").on("click", function () {
+        $("#start").hide();
+        displayQuestion();
+        runTimer();
+        for (var i = 0; i < options.length; i++) {
+            holder.push(options[i]);
+        }
+    })
+    //timer start
+    function runTimer() {
+        if (!running) {
+            intervalId = setInterval(decrement, 1000);
+            running = true;
+        }
+    }
+    //timer countdown
+    function decrement() {
+        $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+        timer--;
+
+        //stop timer if reach 0
+        if (timer === 0) {
+            unanswerCount++;
+            stop();
+            $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }
+    }
+
+    //timer stop
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
+    }
+    //randomly pick question in array if not already shown
+    //display question and loop though and display possible answers
+    function displayQuestion() {
+        //generate random index in array
+        index = Math.floor(Math.random() * options.length);
+        pick = options[index];
+
+        //	if (pick.shown) {
+        //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
+        //		displayQuestion();
+        //	} else {
+        //		console.log(pick.question);
+        //iterate through answer array and display
+        $("#questionblock").html("<h2>" + pick.question + "</h2>");
+        for (var i = 0; i < pick.choice.length; i++) {
+            var userChoice = $("<div>");
+            userChoice.addClass("answerchoice");
+            userChoice.html(pick.choice[i]);
+            //assign array position to it so can check answer
+            userChoice.attr("data-guessvalue", i);
+            $("#answerblock").append(userChoice);
+            //		}
+        }
+
+
+
+        //click function to select answer and outcomes
+        $(".answerchoice").on("click", function () {
+            //grab array position from userGuess
+            userGuess = parseInt($(this).attr("data-guessvalue"));
+
+            //correct guess or wrong guess outcomes
+            if (userGuess === pick.answer) {
+                stop();
+                correctCount++;
+                userGuess = "";
+                $("#answerblock").html("<p>Correct!</p>");
+                hidepicture();
+
+            } else {
+                stop();
+                wrongCount++;
+                userGuess = "";
+                $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+                hidepicture();
+            }
+        })
+    }
+
+
+    function hidepicture() {
+        $("#answerblock").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        options.splice(index, 1);
+
+        var hidpic = setTimeout(function () {
+            $("#answerblock").empty();
+            timer = 20;
+
+            //run the score screen if all questions answered
+            if ((wrongCount + correctCount + unanswerCount) === qCount) {
+                $("#questionblock").empty();
+                $("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+                $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>");
+                $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>");
+                $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>");
+                $("#reset").show();
+                correctCount = 0;
+                wrongCount = 0;
+                unanswerCount = 0;
+
+            } else {
+                runTimer();
+                displayQuestion();
+
+            }
+        }, 3000);
+
+
+    }
+
+    $("#reset").on("click", function () {
+        $("#reset").hide();
+        $("#answerblock").empty();
+        $("#questionblock").empty();
+        for (var i = 0; i < holder.length; i++) {
+            options.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+
+    })
+
+})
